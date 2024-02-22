e15df5a76e52519481ccfe351e871bc4729cbbc9
Thiago <thiaguksianino@gmail.com>
2024-02-22 20:57:42 -0300

    Adding Item entity and ItemValidator with FluentValidations

---
+++ Backend/HahnSoftwareCrud/HahnSoftwareCrud.Application/Validators/ItemValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using HahnSoftwareCrud.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HahnSoftwareCrud.Application.Validators
+{
+    public class ItemValidator : AbstractValidator<Item>
+    {
+        public ItemValidator()
+        {
+            RuleFor(item => item.Name).NotEmpty().WithMessage("Last name is required.");
+            RuleFor(item => item.Name).Length(3,30).WithMessage("Name Lenght must be between 3 and 30 character");
+            RuleFor(item => item.Quantity).InclusiveBetween(1, 99).WithMessage("Quantity must be between 1 and 99.");
+        }
+    }
+}
