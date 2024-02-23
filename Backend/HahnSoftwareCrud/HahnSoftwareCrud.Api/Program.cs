d80ee3cb5548b528da544f0e1b80913ef63e3bc6
Thiago <thiaguksianino@gmail.com>
2024-02-23 12:11:46 -0300

    Configuring CORS policy - Backend

--- Backend/HahnSoftwareCrud/HahnSoftwareCrud.Api/Program.cs
+++ Backend/HahnSoftwareCrud/HahnSoftwareCrud.Api/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
